c26cb9f2a37825aa20697e9b83f93b3630630d67
adnkh <adnan.khabbaz@gmail.com>
2018-08-03 04:37:25 +0300

    added track

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, withRouter } from 'react-router-dom';
 import 'normalize.css';
 import './App.css';
 import Mappage from './components/screens/mappage';
+import TrackMap from './components/screens/TrackMap';
 import QRscan from './components/screens/QRscan';
 import firstpage from './components/screens/firstpage';
 import Profile from './components/screens/Profile';
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <Route path="/Map" component={Mappage} />
+        <Route path="/track" component={TrackMap} />
         <Route path="/QRscan" component={QRscan} />
         <Route path="/firstpage" component={firstpage} />
         <Route path="/Profile" component={Profile} />
